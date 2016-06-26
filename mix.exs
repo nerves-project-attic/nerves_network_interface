8d2a3e772b5eafdfa02c55c546618d182ad61a1e
Frank Hunleth <fhunleth@troodon-software.com>
2016-06-26 10:20:12 -0400

    Bump deps - especially elixir_make for OTP 19 fix

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule Nerves.NetworkInterface.Mixfile do
 
   defp deps do
     [
-      {:elixir_make, "~> 0.1"},
+      {:elixir_make, "~> 0.3"},
       {:earmark, "~> 0.1", only: :dev},
       {:ex_doc, "~> 0.11", only: :dev},
       {:credo, "~> 0.3", only: [:dev, :test]}

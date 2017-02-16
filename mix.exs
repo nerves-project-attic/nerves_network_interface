7fba1f7afc3aa8435ae9a6fcc54e0be90599a5cf
Justin Schneck <jschneck@mac.com>
2017-02-16 15:36:58 -0500

    v0.4.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Nerves.NetworkInterface.Mixfile do
 
   def project do
     [app: :nerves_network_interface,
-     version: "0.4.0-dev",
+     version: "0.4.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,

a451743abcf77c6ceeba0c55c0b3e7c7f9671a72
Frank Hunleth <fhunleth@troodon-software.com>
2017-05-17 09:38:12 -0400

    Don't create the - file in test-c99.sh

--- src/test-c99.sh
+++ src/test-c99.sh
@@ -5,5 +5,5 @@ if [ -z $CC ]; then
 fi
 
 # See Makefile
-printf '#include <arpa/inet.h>\n#include <net/if.h>\n#include <linux/if.h>\nint main(int argc,char*argv[]) { return IFF_UP; }' | $CC -std=c99 -D_XOPEN_SOURCE=600 -o - -xc - 2>/dev/null && printf "yes"
+printf '#include <arpa/inet.h>\n#include <net/if.h>\n#include <linux/if.h>\nint main(int argc,char*argv[]) { return IFF_UP; }' | $CC -std=c99 -D_XOPEN_SOURCE=600 -o /dev/null -xc - 2>/dev/null && printf "yes"
 

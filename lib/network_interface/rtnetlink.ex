c91dd67ab534a3e73586f69e41b5cb0f9544face
Justin Schneck <jschneck@mac.com>
2017-05-17 14:47:52 -0400

    updated tests to return system registry transactions (#6)
    * updated tests to return system registry transactions
    
    * Wired port messages into system registry
    
    * Added dellink handling

--- lib/network_interface/rtnetlink.ex
+++ lib/network_interface/rtnetlink.ex
@@ -1,26 +1,127 @@
 defmodule Nerves.NetworkInterface.Rtnetlink do
 
+  alias SystemRegistry, as: SR
+  alias SystemRegistry.Transaction, as: T
+
+  def decode(messages, ifaces) when is_list(messages) do
+    Enum.reduce(messages, {:ok, T.begin, ifaces}, fn
+      (msg, {error, t, ifaces}) ->
+        case decode(t, msg, ifaces) do
+          {:ok, t, ifaces} -> {error, t, ifaces}
+          {:noop, t, ifaces} -> {error, t, ifaces}
+          {:error, _, ifaces} -> {:error, t, ifaces}
+        end
+    end)
+  end
+
+  def decode(msg, ifaces) do
+    decode(T.begin, msg, ifaces)
+  end
+
   @doc """
   Convert newlink notifications into updates.
   """
-  def newlink(%{index: ifnum} = message) do
-    new_message = Map.delete(message, :index)
-    {[:state, :network_interface, ifnum], new_message}
+  def decode(%T{} = t, {:newlink, msg}, ifaces) do
+    iface = msg
+    {_, ifaces} =
+      iface(ifaces, iface.index)
+
+
+    t = SR.update(t, [:state, :network_interface, iface.ifname], iface)
+    {:ok, t, [iface | ifaces]}
+  end
+
+  def decode(%T{} = t, {:dellink, msg}, ifaces) do
+    iface = msg
+    {_, ifaces} = iface(ifaces, msg.index)
+    t = SR.delete(t, [:state, :network_interface, iface.ifname])
+    {:ok, t, ifaces}
   end
 
   @doc """
   Convert newaddr notifications into updates.
   """
-  def newaddr(%{index: ifnum, address: address} = message) do
-    new_message = message
-    |> Map.delete(:index)
-    |> Map.delete(:address)
-    |> Map.delete(:label)
+  def decode(%T{} = t, {:newaddr, msg}, ifaces) do
+    case iface(ifaces, msg.index) do
+      {[iface], ifaces} ->
+        addr_key = msg.address
+        address = prune_address(msg)
 
-    {[:state, :network_interface, ifnum, :addresses, address], new_message}
+        addresses =
+          iface
+          |> Map.get(:addresses, %{})
+          |> Map.put(addr_key, address)
+
+        iface = Map.put(iface, :addresses, addresses)
+        t = SR.update(t, [:state, :network_interface, iface.ifname, :addresses, addr_key], address)
+
+        {:ok, t, [iface | ifaces]}
+      _ ->
+        {:error, "unknown interface at index: #{msg.index}", ifaces}
+    end
+  end
+
+  @doc """
+  Convert newaddr notifications into updates.
+  """
+  def decode(%T{} = t, {:deladdr, msg}, ifaces) do
+    case iface(ifaces, msg.index) do
+      {[iface], ifaces} ->
+        addr_key = msg.address
+
+        addresses =
+          iface
+          |> Map.get(:addresses, %{})
+          |> Map.delete(addr_key)
+
+        iface = Map.put(iface, :addresses, addresses)
+        t = SR.delete(t, [:state, :network_interface, iface.ifname, :addresses, addr_key])
+
+        {:ok, t, [iface | ifaces]}
+      _ ->
+        {:error, "unknown interface at index: #{msg.index}", ifaces}
+    end
   end
 
   @doc """
   Convert newroute notifications into updates.
   """
+  def decode(%T{} = t, {:newroute, msg}, ifaces) do
+    case iface(ifaces, msg.oif) do
+      {[iface], ifaces} ->
+        {_, routes} = route(Map.get(iface, :routes, []), msg)
+        route = prune_route(msg)
+        routes = [route | routes]
+        gateway = Map.get(route, :gateway)
+        iface =
+          iface
+          |> Map.put(:routes, routes)
+          |> Map.put(:gateway, gateway)
+        t =
+          t
+          |> SR.update([:state, :network_interface, iface.ifname, :routes], routes)
+          |> SR.update([:state, :network_interface, iface.ifname, :gateway], gateway)
+        {:ok, t, [iface | ifaces]}
+      _ -> {:error, "unknown interface at index: #{msg.oif}", ifaces}
+    end
+  end
+
+  def decode(t, _, ifaces), do: {:noop, t, ifaces}
+
+  defp iface(ifaces, ifindex),
+    do: Enum.split_with(ifaces, & &1.index == ifindex)
+
+  defp route(routes, route),
+    do: Enum.split_with(routes, & &1 == route)
+
+  defp prune_address(address) do
+    address
+    |> Map.delete(:index)
+    |> Map.delete(:label)
+  end
+
+  defp prune_route(route) do
+    route
+    |> Map.delete(:oif)
+  end
 end
--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule Nerves.NetworkInterface.Mixfile do
   end
 
   defp deps do
-    [{:system_registry, github: "mobileoverlord/system_registry"},
+    [{:system_registry, "~> 0.1"},
      {:elixir_make, "~> 0.4", runtime: false},
      {:ex_doc, "~> 0.11", only: :dev}]
   end

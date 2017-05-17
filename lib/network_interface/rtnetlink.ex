ea3ce4d2965d8fbbd070c4e0772c6f1f656919fc
Justin Schneck <jschneck@mac.com>
2017-05-17 12:33:59 -0400

    updated tests to return system registry transactions

--- lib/network_interface/rtnetlink.ex
+++ lib/network_interface/rtnetlink.ex
@@ -1,26 +1,108 @@
 defmodule Nerves.NetworkInterface.Rtnetlink do
 
+  alias SystemRegistry, as: SR
+
   @doc """
   Convert newlink notifications into updates.
   """
-  def newlink(%{index: ifnum} = message) do
-    new_message = Map.delete(message, :index)
-    {[:state, :network_interface, ifnum], new_message}
+  def newlink(ifaces, %{ifname: ifname} = iface) do
+    {_, ifaces} =
+      iface(ifaces, iface.index)
+
+    t =
+      SR.transaction
+      |> SR.update([:state, :network_interface, ifname], iface)
+    {:ok, {[iface | ifaces], t}}
   end
 
   @doc """
   Convert newaddr notifications into updates.
   """
-  def newaddr(%{index: ifnum, address: address} = message) do
-    new_message = message
-    |> Map.delete(:index)
-    |> Map.delete(:address)
-    |> Map.delete(:label)
+  def newaddr(ifaces, address) do
+    case iface(ifaces, address.index) do
+      {[iface], ifaces} ->
+        addr_key = address.address
+        address = prune_address(address)
 
-    {[:state, :network_interface, ifnum, :addresses, address], new_message}
+        addresses =
+          iface
+          |> Map.get(:addresses, %{})
+          |> Map.put(addr_key, address)
+
+        iface = Map.put(iface, :addresses, addresses)
+        t =
+          SR.transaction
+          |> SR.update([:state, :network_interface, iface.ifname, :addresses, addr_key], address)
+        reply =
+          {[iface | ifaces], t}
+        {:ok, reply}
+      _ ->
+        {:error, "unknown interface at index: #{address.index}"}
+    end
+  end
+
+  @doc """
+  Convert newaddr notifications into updates.
+  """
+  def deladdr(ifaces, address) do
+    case iface(ifaces, address.index) do
+      {[iface], ifaces} ->
+        addr_key = address.address
+
+        addresses =
+          iface
+          |> Map.get(:addresses, %{})
+          |> Map.delete(addr_key)
+
+        iface = Map.put(iface, :addresses, addresses)
+        t =
+          SR.transaction
+          |> SR.delete([:state, :network_interface, iface.ifname, :addresses, addr_key])
+        reply =
+          {[iface | ifaces], t}
+        {:ok, reply}
+      _ ->
+        {:error, "unknown interface at index: #{address.index}"}
+    end
   end
 
   @doc """
   Convert newroute notifications into updates.
   """
+  def newroute(ifaces, route) do
+    case iface(ifaces, route.oif) do
+      {[iface], ifaces} ->
+        {_, routes} = route(Map.get(iface, :routes, []), route)
+        route = prune_route(route)
+        routes = [route | routes]
+        gateway = Map.get(route, :gateway)
+        iface =
+          iface
+          |> Map.put(:routes, routes)
+          |> Map.put(:gateway, gateway)
+        t =
+          SR.transaction
+          |> SR.update([:state, :network_interface, iface.ifname, :routes], routes)
+          |> SR.update([:state, :network_interface, iface.ifname, :gateway], gateway)
+        {:ok, {[iface | ifaces], t}}
+      _ -> {:error, "unknown interface at index: #{route.oif}"}
+    end
+  end
+
+  defp iface(ifaces, ifindex),
+    do: Enum.split_with(ifaces, & &1.index == ifindex)
+
+  defp route(routes, route),
+    do: Enum.split_with(routes, & &1 == route)
+
+  defp prune_address(address) do
+    address
+    |> Map.delete(:index)
+    |> Map.delete(:label)
+  end
+
+  defp prune_route(route) do
+    route
+    |> Map.delete(:oif)
+  end
 end
--- test/rtnetlink_test.exs
+++ test/rtnetlink_test.exs
@@ -3,7 +3,6 @@ defmodule RtnetlinkTest do
   alias Nerves.NetworkInterface.Rtnetlink, as: R
 
   test "newlink returns system registry update tuple" do
-    state = %{}
 
     # The idea here is that this module knows how to translate rtnetlink messages
     # into the appropriate system registry key value pairs to pass to update.
@@ -20,66 +19,65 @@ defmodule RtnetlinkTest do
     # updates. On initialization, I'll dump all addresses and routes to
     # build up the initial state.
     #
-
-    {newstate, k, v} = R.newlink(state, %{ifname: "wlan0", index: 3, is_broadcast: true})
-
-    assert k == [:state, :network_interface, 3]
-    assert v == %{ifname: "wlan0", is_broadcast: true}
+    ifaces = []
+    {:ok, {_, t}} = R.newlink(ifaces, %{ifname: "wlan0", index: 3, is_broadcast: true})
+    iface = get_in(t.updates, [:state, :network_interface, "wlan0"])
+    assert iface == %{ifname: "wlan0", is_broadcast: true, index: 3}
   end
 
   test "first newaddr returns system registry update with an address" do
-    state = %{}
-    {newstate, k, v} = R.newaddr(state, %{address: "192.168.1.15", index: 3})
-
-    assert {k, v} == {[:state, :network_interface, 3, :addresses, 0], %{address: "192.168.1.15"}}
+    ifaces = [%{ifname: "wlan0", index: 3, is_broadcast: true}]
+    {:ok, {_ifaces, t}} = R.newaddr(ifaces, %{address: "192.168.1.15", index: 3})
+    address = get_in(t.updates, [:state, :network_interface, "wlan0", :addresses, "192.168.1.15"])
+    assert address == %{address: "192.168.1.15"}
   end
 
   test "newaddr drops label attribute" do
-    state = %{}
-    {newstate, k, v} = R.newaddr(state, %{address: "192.168.1.15", index: 3, label: "wlan0"})
-
-    assert {k, v} == {[:state, :network_interface, 3, :addresses, 0], %{address: "192.168.1.15"}}
+    ifaces = [%{ifname: "wlan0", index: 3, is_broadcast: true}]
+    {:ok, {_ifaces, t}} = R.newaddr(ifaces, %{address: "192.168.1.15", index: 3, label: "wlan0"})
+    address = get_in(t.updates, [:state, :network_interface, "wlan0", :addresses, "192.168.1.15"])
+    assert address == %{address: "192.168.1.15"}
   end
 
   test "newaddr adds all attributes" do
-    state = %{}
-
-    {newstate, k, v} = R.newaddr(state, %{address: "192.168.1.15", broadcast: "192.168.1.255", family: :af_inet, index: 3, label: "wlan0", local: "192.168.1.15", prefixlen: 24, scope: 0})
-
-    assert {k, v} ==
-      {[:state, :network_interface, 3, :addresses, 0], %{address: "192.168.1.15", broadcast: "192.168.1.255", family: :af_inet, local: "192.168.1.15", prefixlen: 24, scope: 0}}
+    ifaces = [%{ifname: "wlan0", index: 3, is_broadcast: true}]
+    {:ok, {_ifaces, t}} = R.newaddr(ifaces, %{address: "192.168.1.15", broadcast: "192.168.1.255", family: :af_inet, index: 3, label: "wlan0", local: "192.168.1.15", prefixlen: 24, scope: 0})
+    address = get_in(t.updates, [:state, :network_interface, "wlan0", :addresses, "192.168.1.15"])
+    assert address == %{address: "192.168.1.15", broadcast: "192.168.1.255", family: :af_inet, local: "192.168.1.15", prefixlen: 24, scope: 0}
   end
 
   test "deladdr returns a system registry key to delete" do
-    state = %{}
-    {state, _, _} = R.newaddr(state, %{address: "fe80::863a:4bff:fe11:95f6", family: :af_inet6, index: 3, prefixlen: 64, scope: 253})
-    {state, k} = R.deladdr(state, %{address: "fe80::863a:4bff:fe11:95f6", family: :af_inet6, index: 3, prefixlen: 64, scope: 253})
-
-    assert k == [:state, :network_interface, 3, :addresses, 0]
+    ifaces = [%{ifname: "wlan0", index: 3, is_broadcast: true}]
+    {:ok, {ifaces, _t}} = R.newaddr(ifaces, %{address: "fe80::863a:4bff:fe11:95f6", family: :af_inet6, index: 3, prefixlen: 64, scope: 253})
+    {:ok, {_ifaces, t}} = R.deladdr(ifaces, %{address: "fe80::863a:4bff:fe11:95f6", family: :af_inet6, index: 3, prefixlen: 64, scope: 253})
+    assert Enum.any?(t.deletes, & &1.node == [:state, :network_interface, "wlan0", :addresses, "fe80::863a:4bff:fe11:95f6"])
   end
 
-  test "second newaddr returns system registry update with index 1" do
-    state = %{}
-    {state, _, _} = R.newaddr(state, %{address: "192.168.1.15", index: 3})
-    {state, k, v} = R.newaddr(state, %{address: "fe80::863a:4bff:fe11:95f6", index: 3})
-
-    assert {k, v} == {[:state, :network_interface, 3, :addresses, 1], %{address: "fe80::863a:4bff:fe11:95f6"}}
+  test "interface can support multiple addresses" do
+    ifaces = [%{ifname: "wlan0", index: 3, is_broadcast: true}]
+    {:ok, {ifaces, _t}} = R.newaddr(ifaces, %{address: "192.168.1.15", index: 3})
+    {:ok, {ifaces, _t}} = R.newaddr(ifaces, %{address: "fe80::863a:4bff:fe11:95f6", index: 3})
+    iface = Enum.find(ifaces, & &1.ifname == "wlan0")
+    assert Enum.any?(iface.addresses, fn({addr, _}) -> addr == "192.168.1.15" end)
+    assert Enum.any?(iface.addresses, fn({addr, _}) -> addr == "fe80::863a:4bff:fe11:95f6" end)
   end
 
 
   test "newroute returns system registry default gateway update" do
-    state = %{}
-    {state, k, v} = R.newroute(state, %{oif: 3, gateway: "192.168.1.1"})
-
-    assert {k, v} ==
-      {[:state, :network_interface, 3, :gateways, "192.168.1.1"], %{}}
+    ifaces = [%{ifname: "wlan0", index: 3, is_broadcast: true}]
+    {:ok, {_ifaces, t}} = R.newroute(ifaces, %{oif: 3, gateway: "192.168.1.1"})
+    routes = get_in(t.updates, [:state, :network_interface, "wlan0", :routes])
+    gateway = get_in(t.updates, [:state, :network_interface, "wlan0", :gateway])
+    assert routes == [%{gateway: "192.168.1.1"}]
+    assert gateway == "192.168.1.1"
   end
-  test "newroute without a gateway does nothing" do
-    state = %{}
 
-    {state, k, v} = R.newroute(state, %{dst: "192.168.1.0", family: :af_inet, oif: 3, prefsrc: "192.168.1.15", priority: 600, protocol: :kernel, scope: :link, table: :main, tos: 0, type: :unicast})
+  test "newroute without a gateway" do
+    ifaces = [%{ifname: "wlan0", index: 3, is_broadcast: true}]
 
-    assert {k, v} == {:none, :none}
+    {:ok, {_, t}} = R.newroute(ifaces, %{dst: "192.168.1.0", family: :af_inet, oif: 3, prefsrc: "192.168.1.15", priority: 600, protocol: :kernel, scope: :link, table: :main, tos: 0, type: :unicast})
+    gateway = get_in(t.updates, [:state, :network_interface, "wlan0", :gateway])
+    assert gateway == nil
   end
 
   test "dellink returns a system registry key to delete" do

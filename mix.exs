c0778066c10291f5b716f7e5dc6fcd3e261c7f53
Frank Hunleth <fhunleth@troodon-software.com>
2017-11-23 11:58:33 -0500

    Bump version for new work

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Nerves.NetworkInterface.Mixfile do
 
   def project do
     [app: :nerves_network_interface,
-     version: "0.4.3",
+     version: "0.4.4-dev",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,

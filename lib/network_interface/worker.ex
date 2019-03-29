2914e845804e4770ce02d9c141c34c2c92d95ead
Tomasz Kazimierz Motyl <tomasz.motyl@se.com>
2019-03-29 03:25:11 -0700

    Correcting syntax error

--- lib/network_interface/worker.ex
+++ lib/network_interface/worker.ex
@@ -188,7 +188,7 @@ defmodule Nerves.NetworkInterface.Worker do
     Logger.debug "nerves_network_interface received #{inspect notif} and #{inspect data}"
     Registry.dispatch(Nerves.NetworkInterface, data.ifname, fn entries ->
       for {pid, _} <- entries do
-        Logger.debug("handle_info: Dispatching for pid = #{inspect pid} notif = #{inspect notif} data = #{inspect data}"
+        Logger.debug("handle_info: Dispatching for pid = #{inspect pid} notif = #{inspect notif} data = #{inspect data}")
         Logger.debug("Process info for pid = #{inspect pid}: #{inspect Process.info(pid)}")
 
         send(pid, {Nerves.NetworkInterface, notif, data})

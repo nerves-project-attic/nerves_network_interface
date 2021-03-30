870117a315f2afa9c58993e97fef236490677d8f
Tomasz Kazimierz Motyl <tomasz.motyl@se.com>
2021-03-30 05:50:31 -0700

    Splitting the debug related declarations into separate header file

---
+++ src/debug.h
@@ -0,0 +1,36 @@
+/****************************************************************
+ * Copyright (C) 2021 Schneider Electric                        *
+ ****************************************************************/
+
+#if !defined(__DEBUG_H__)
+#define __DEBUG_H__
+
+//#define DEBUG
+//#define INFO
+//#define ERROR
+
+#ifdef DEBUG
+#define debug(format, ...)  fprintf(stderr, "DBG: "format, __VA_ARGS__); fprintf(stderr, "\r\n")
+#define debugf(string) fprintf(stderr, string); fprintf(stderr, "\r\n")
+#else
+#define debug(format, ...)
+#define debugf(string)
+#endif
+
+#ifdef INFO
+#define info(format, ...)  fprintf(stderr, "INFO: "format, __VA_ARGS__); fprintf(stderr, "\r\n")
+#define infof(string) fprintf(stderr, string); fprintf(stderr, "\r\n")
+#else
+#define info(format, ...)
+#define infof(string)
+#endif
+
+#ifdef ERROR
+#define error(format, ...)  fprintf(stderr, "ERROR:"format, __VA_ARGS__); fprintf(stderr, "\r\n")
+#define errorf(string) fprintf(stderr, string); fprintf(stderr, "\r\n")
+#else
+#define error(format, ...)
+#define errorf(string)
+#endif
+
+#endif
